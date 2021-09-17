a7b5cc2b7d579ae0ccf316f1e819568176358238
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-09-17 23:59:16 +0530

    routes declaration

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,19 @@ const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
+//requests
 
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hello from Server'
+    })
+})
+
+app.get('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    })
+})
 
 app.listen(port,host, () => {
     console.log(`Server is running on ${host}:${port}`);
