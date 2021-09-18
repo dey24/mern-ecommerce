82697356c36ed9859f86f32ec7a2837b672090d3
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-09-18 11:32:16 +0530

    database successfully connected

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,29 @@ const env = require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
+const user = process.env.MONGODB_USER
+const pwd = process.env.MONGODB_PASSWORD
+const db = process.env.MONGODB_DB
+const mongoose = require('mongoose');
+//connecting database
+
+mongoose.connect(`mongodb+srv://${user}:${pwd}@cluster0.xrml9.mongodb.net/${db}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('Database connected successfully');
+    });
+
+//middleware
+
+app.use(express.json());
 
 //requests
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Hello from Server'
+        message: 'Konichiwa from Server'
     })
 })
 
