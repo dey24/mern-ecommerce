aaab8c8ca97fa94a2f72a2fc85468dfa5aec7806
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-09-28 21:54:49 +0530

    User model created

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express'); 
 const router = express.Router();
-var User = require('../models/users')
+
 
 router.post('/signin', (req, res) =>{
     res.send("user signin is successful");
 })
 
 router.post('/signup', (req, res) =>{
-
+    const username = req.body.username;
+    res.send("you username is " + username);
 })
 
 module.exports = router;
