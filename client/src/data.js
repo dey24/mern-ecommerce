124d58c482fdd1265f0ac4c04f298711f0e0a91a
Nilosree Sengupta <nilosreesengupta6@gmail.com>
2021-10-06 22:15:23 +0530

    Components and Pages build up

---
+++ client/src/data.js
@@ -0,0 +1,85 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/0259/6736/2107/products/red-raw-silk-saree-with-gold-cutwork-lace-bordersareehouse-of-blouse-25175011.jpg?v=1588823714",
+      title: "FESTIVE SALE",
+      desc: "THIS FESTIVE SEASON GRAB YOUR STYLE! GET FREE DELIVERY AND GET FLAT 50% OFF FOR NEW ARRIVALS ",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://m.media-amazon.com/images/I/7180HlP6u9L._SL1500_.jpg",
+      title: "HEADPHONES",
+      desc: "MATCH YOUR STYLE! GET FLAT 10% OFF ON SELECT CREDIT CARDS.",
+      //bg: "fcf1ed",
+      bg: "edf8fc",
+    },
+    {
+      id: 3,
+      img: "http://www.uttarpradesh.org/wp-content/uploads/2017/10/vegetables.jpg",
+      title: "FRESH VEGIES",
+      desc: "GET FRESH VEGIES FOR YOUR HEALTH! FREE DELIVERY AT DOOR STEP!",
+      bg: "edfcf1",
+    },
+    {
+      id: 4,
+      img: "http://www.uttarpradesh.org/wp-content/uploads/2017/10/vegetables.jpg",
+      title: "FRESH VEGIES",
+      desc: "GET FRESH VEGIES FOR YOUR HEALTH! FREE DELIVERY AT DOOR STEP!",
+      bg: "edfcf1",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1364628/2016/8/31/11472636737718-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-1.jpg",
+      title: "MENS' WEAR!",
+    },
+    {
+      id: 2,
+      img: "https://cdn.shopify.com/s/files/1/0526/4811/6407/products/TASS1940_2_1800x1800.jpg?v=1616333918",
+      title: "WOMENS' WEAR",
+    },
+    {
+      id: 3,
+      img: "https://i.etsystatic.com/8160195/r/il/574b61/723699232/il_1588xN.723699232_hsga.jpg",
+      title: "KIDS' WEAR",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"",
+      
+    },
+    {
+      id:2,
+      img:"",
+    },
+    {
+      id:3,
+      img:"",
+    },
+    {
+      id:4,
+      img:"",
+    },
+    {
+      id:5,
+      img:"",
+    },
+    {
+      id:6,
+      img:"",
+    },
+    {
+      id:7,
+      img:"",
+    },
+    {
+      id:8,
+      img:"",
+    },
+  ]
