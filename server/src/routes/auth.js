8436e10b08892e9ff33a7bb2efde1389540ae00a
Chiranjit Dey <chiranjitdey2000@gmail.com>
2021-10-28 19:29:14 +0530

    passwords encrypted

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express'); 
 const router = express.Router();
 const User = require('../models/User');
+const CryptoJS = require("crypto-js");
 //Regsiter
 router.post("/register", async(req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     })
     try{
         const savedUser = await newUser.save();
